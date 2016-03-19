863f8390e5c024efd49cf8427cc440514ac5d815
defrifajarutomo <defriblackertz@gmail.com>
2016-03-19 22:36:56 +0700

    update cors

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,28 +6,8 @@ Route::get('/', function () {
 
 Route::resource('tasks', 'TaskController', ['except' => ['create', 'show']]);
 
-Route::group(['prefix' => 'api/v1/ajax/'], function() {
+Route::group(['prefix' => 'api/v1/ajax/', 'middleware' => ['cors']], function() {
 	Route::post('tasks', 'AjaxController@tasks');
-});
-
-
-Route::get('test', function() {
-	$routeCollection = Route::getRoutes();
-
-echo "<table style='width:100%'>";
-    echo "<tr>";
-        echo "<td width='10%'><h4>HTTP Method</h4></td>";
-        echo "<td width='10%'><h4>Route</h4></td>";
-        echo "<td width='80%'><h4>Corresponding Action</h4></td>";
-    echo "</tr>";
-    foreach ($routeCollection as $value) {
-        echo "<tr>";
-            echo "<td>" . $value->getMethods()[0] . "</td>";
-            echo "<td>" . $value->getPath() . "</td>";
-            echo "<td>" . $value->getActionName() . "</td>";
-        echo "</tr>";
-    }
-echo "</table>";
 });
 
 
